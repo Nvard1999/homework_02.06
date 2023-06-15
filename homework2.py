edb4a1406946492b912b7e5f686ac11e40b6b526
Nvard1999 <99895481+Nvard1999@users.noreply.github.com>
2023-06-15 14:33:49 +0400

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,58 +1,58 @@
-# Suppose you have a 5 × 5 list that consists of zeros and M’s. Write a program that creates a new 5 × 5 list that has M’s in the same place, but the zeroes are replaced by counts of how many M’s are in adjacent cells (adjacent either horizontally, vertically, or diagonally). An example is shown below. [Hint: short-circuiting may be helpful for avoiding index-out-of-range errors.]
+ Suppose you have a 5 × 5 list that consists of zeros and M’s. Write a program that creates a new 5 × 5 list that has M’s in the same place, but the zeroes are replaced by counts of how many M’s are in adjacent cells (adjacent either horizontally, vertically, or diagonally). An example is shown below. [Hint: short-circuiting may be helpful for avoiding index-out-of-range errors.]
 
 
-# lst = [
-# 	[0,'M',0,0,'M'],
-# 	['M',0,'M',0,'M'],
-# 	[0,0,'M',0,'M'],
-# 	['M','M',0,'M',0],
-# 	['M',0,0,'M',0]
-# ]
-# new_lst = [[0]*len(lst[0])for _ in range(len(lst))]
-# count_lst = []
-# for i in lst:
-# 	count = 0
-# 	for j in i:
-# 		if j == 'M':
-# 			count += 1
-# 	count_lst.append(count)
-# for i in range(len(lst)):
-# 	for j in range(len(lst[0])):
-# 		if lst[i][j] == 'M':
-# 			new_lst[i][j] = 'M'
-# 		else:
-# 			new_lst[i][j] = count_lst[i]
-# for i in new_lst:
-# 	print(i)
-
-
-
-
-
-# Write a program that rotates a matrix by 180 degrees.
-
-
-# def rotate_180(matrix):
-# 	rows = len(matrix)
-# 	columns = len(matrix[0])
-# 	rotated_matrix = [[0] * columns for _ in range(rows)]
-# 	for i in range(rows):
-# 		for j in range(columns):
-# 			rotated_matrix[rows - i - 1][columns - j - 1] = matrix[i][j]
-# 	return rotated_matrix
-# matrix = [[1,2,3,4],
-#           [5,6,7,8],
-#           [9,10,11,12],
-#           [13,14,15,16]]
-# rotated_matrix = rotate_180(matrix)
-# for row in rotated_matrix:
-# 	print(row)
+ lst = [
+ 	[0,'M',0,0,'M'],
+ 	['M',0,'M',0,'M'],
+ 	[0,0,'M',0,'M'],
+ 	['M','M',0,'M',0],
+ 	['M',0,0,'M',0]
+        ]
+ new_lst = [[0]*len(lst[0])for _ in range(len(lst))]
+ count_lst = []
+ for i in lst:
+ 	count = 0
+ 	for j in i:
+ 		if j == 'M':
+ 			count += 1
+ 	count_lst.append(count)
+ for i in range(len(lst)):
+ 	for j in range(len(lst[0])):
+ 		if lst[i][j] == 'M':
+ 			new_lst[i][j] = 'M'
+ 		else:
+ 			new_lst[i][j] = count_lst[i]
+ for i in new_lst:
+ 	print(i)
 
 
 
 
 
-# Write a program that asks the user for an hour between 1 and 12, asks them to enter am or pm, and asks them how many hours into the future they want to go. Print out what the hour will be that many hours into the future, printing am or pm as appropriate. An example is shown below.
+ Write a program that rotates a matrix by 180 degrees.
+
+
+ def rotate_180(matrix):
+ 	rows = len(matrix)
+ 	columns = len(matrix[0])
+ 	rotated_matrix = [[0] * columns for _ in range(rows)]
+	for i in range(rows):
+ 		for j in range(columns):
+ 			rotated_matrix[rows - i - 1][columns - j - 1] = matrix[i][j]
+ 	return rotated_matrix
+ matrix = [[1,2,3,4],
+           [5,6,7,8],
+           [9,10,11,12],
+           [13,14,15,16]]
+ rotated_matrix = rotate_180(matrix)
+ for row in rotated_matrix:
+ 	print(row)
+
+
+
+
+
+ Write a program that asks the user for an hour between 1 and 12, asks them to enter am or pm, and asks them how many hours into the future they want to go. Print out what the hour will be that many hours into the future, printing am or pm as appropriate. An example is shown below.
 
 
 
